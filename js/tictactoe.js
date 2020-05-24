0973810f489a1b9870703228d4d3872b9a27a951
Michał Rosa <michal.dew@gmail.com>
2020-05-24 19:04:06 +0200

    imlement board size changing from 3x3 to 5x5

--- js/tictactoe.js
+++ js/tictactoe.js
@@ -13,10 +13,11 @@ let $board;
 let $winningMessage;
 let $winningMessageText;
 let $winningMessageButton;
+let $boardSizeSelect;
 
 let $currentMove = $moveX;
 let $moveCounter = 0;
-const $boardSize = 3;
+let $boardSize = 3;
 
 const main = () => {
     prepareDOMElements();
@@ -25,12 +26,13 @@ const main = () => {
 
 const prepareDOMElements = () => {
     $board = document.querySelector('.board--js');
-    $boardCells = document.querySelectorAll('.board__cell--js');
     $winningMessage = document.querySelector('.winning-message--js');
     $winningMessageText = document.querySelector('.winning-message__text--js');
     $winningMessageButton = document.querySelector('.winning-message__button--js');
+    $boardSizeSelect = document.querySelector('.board-size--js');
 
     $winningMessageButton.addEventListener('click', newGame);
+    $boardSizeSelect.addEventListener('change', reset);
 }
 
 const newGame = () => {
@@ -38,6 +40,7 @@ const newGame = () => {
 }
 
 const reset = () => {
+    generateCells();
     $boardCells.forEach(cell => {
         cell.classList.remove($markOClass);
         cell.classList.remove($markXClass);
@@ -55,6 +58,20 @@ const reset = () => {
     $board.classList.add($currentMove);
 }
 
+const generateCells = () => {
+    $boardSize = parseInt($boardSizeSelect.value, 10);
+    $board.innerHTML = '';
+    $board.style.setProperty('--board-size', $boardSize);
+
+    for (let i = 0; i < $boardSize * $boardSize; i++) {
+        const cell = document.createElement('div');
+        cell.classList.add('board__cell', 'board__cell--js');
+        $board.appendChild(cell);
+    }
+
+    $boardCells = document.querySelectorAll('.board__cell--js');
+}
+
 const markCell = (event) => {
     $moveCounter++;
     putMark(event.target);
@@ -69,101 +86,96 @@ const putMark = cell => {
 }
 
 const checkWin = () => {
-    let xCells = [];
-    let oCells = [];
-    let emptyCells = [];
-    debugger;
+    let xCells = 0;
+    let oCells = 0;
+    
     // check rows
     for (let i = 0; i < $boardSize; i++) {
-        xCells = [];
-        oCells = [];
-        emptyCells = [];
+        xCells = 0;
+        oCells = 0;
         for (let j = 0; j < $boardSize; j++) {
             let cellIndex = i * $boardSize + j;
             if ($boardCells[cellIndex].classList.contains($markXClass)) {
-                xCells.push(cellIndex);
+                xCells++;
             } else if ($boardCells[cellIndex].classList.contains($markOClass)) {
-                oCells.push(cellIndex);
-            } else {
-                emptyCells.push(cellIndex);
+                oCells++;
             }
         }
 
-        if (xCells.length === $boardSize) {
+        if (xCells === $boardSize) {
             showWinningMessage($gameResult.XWIN);
-        } else if (oCells.length === $boardSize) {
+            return;
+        } else if (oCells === $boardSize) {
             showWinningMessage($gameResult.OWIN);
+            return;
         }
     }
 
     // columns
     for (let i = 0; i < $boardSize; i++) {
-        xCells = [];
-        oCells = [];
-        emptyCells = [];
+        xCells = 0;
+        oCells = 0;
         for (let j = 0; j < $boardSize; j++) {
             let cellIndex = i + j * $boardSize;
             if ($boardCells[cellIndex].classList.contains($markXClass)) {
-                xCells.push(cellIndex);
+                xCells++;
             } else if ($boardCells[cellIndex].classList.contains($markOClass)) {
-                oCells.push(cellIndex);
-            } else {
-                emptyCells.push(cellIndex);
+                oCells++;
             }
         }
 
-        if (xCells.length === $boardSize) {
+        if (xCells === $boardSize) {
             showWinningMessage($gameResult.XWIN);
-        } else if (oCells.length === $boardSize) {
+            return;
+        } else if (oCells === $boardSize) {
             showWinningMessage($gameResult.OWIN);
+            return;
         }
     }
 
     // diagonals
-    xCells = [];
-    oCells = [];
-    emptyCells = [];
+    xCells = 0;
+    oCells = 0;
     for (let i = 0; i < $boardSize; i++) {
         let cellIndex = i + i * $boardSize;
         if ($boardCells[cellIndex].classList.contains($markXClass)) {
-            xCells.push(cellIndex);
+            xCells++;
         } else if ($boardCells[cellIndex].classList.contains($markOClass)) {
-            oCells.push(cellIndex);
-        } else {
-            emptyCells.push(cellIndex);
+            oCells++;
         }
     }
 
-    if (xCells.length === $boardSize) {
+    if (xCells === $boardSize) {
         showWinningMessage($gameResult.XWIN);
-    } else if (oCells.length === $boardSize) {
+        return;
+    } else if (oCells === $boardSize) {
         showWinningMessage($gameResult.OWIN);
+        return;
     }
 
-    xCells = [];
-    oCells = [];
-    emptyCells = [];
+    xCells = 0;
+    oCells = 0;
     for (let i = 1; i <= $boardSize; i++) {
         let cellIndex = i * $boardSize - i;
         if ($boardCells[cellIndex].classList.contains($markXClass)) {
-            xCells.push(cellIndex);
+            xCells++;
         } else if ($boardCells[cellIndex].classList.contains($markOClass)) {
-            oCells.push(cellIndex);
-        } else {
-            emptyCells.push(cellIndex);
+            oCells++;
         }
     }
 
-    if (xCells.length === $boardSize) {
+    if (xCells === $boardSize) {
         showWinningMessage($gameResult.XWIN);
-    } else if (oCells.length === $boardSize) {
+        return;
+    } else if (oCells === $boardSize) {
         showWinningMessage($gameResult.OWIN);
+        return;
     }
 
 }
 
 const checkDraw = () => {
-    if ($moveCounter === 9) {
+    if ($moveCounter === ($boardSize * $boardSize)) {
         showWinningMessage($gameResult.DRAW);
     }
 }
